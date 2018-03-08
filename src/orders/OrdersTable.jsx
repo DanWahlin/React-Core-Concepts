a535ef994aa1c34057a0bfc9a484651381ad57d1
Magnus Holm <judofyr@gmail.com>
2018-03-08 15:11:23 +0100

    OrdersTable: Move row rendering inline
    I understand the need for having a separate OrderRow, but personally I
    always start with having an inline renderer for rows, and then refactor
    it out into its own file when it grows.
    
    Many will probably disagree with me, but I think the pattern of having
    multiple render-methods inside a single component is useful to break up
    a big render() without having to define separate components. One of the
    advantages of React's virtual DOM is that it is just regular JavaScript
    values and you can structure it using functions.

--- src/orders/OrdersTable.jsx
+++ src/orders/OrdersTable.jsx
@@ -1,9 +1,7 @@
 // React
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-// Components
-import OrderRow from './OrderRow';
+import Currency from 'react-currency-formatter';
 
 class OrdersTable extends Component {
   static propTypes = {
@@ -14,16 +12,19 @@ class OrdersTable extends Component {
     return (
       <table className="table table-striped table-hover orders-table">
         <tbody>
-          {this.props.orderItems.map(orderItem => (
-            <OrderRow
-              key={orderItem.id}
-              productName={orderItem.productName}
-              itemCost={orderItem.itemCost}
-            />
-          ))}
+          {this.props.orderItems.map(this.renderOrder)}
         </tbody>
       </table>
     );
+  }
+
+  renderOrder(orderItem) {
+    return <tr key={orderItem.id}>
+      <td>{orderItem.productName}</td>
+      <td>
+        <Currency quantity={orderItem.itemCost} />
+      </td>
+    </tr>
   }
 }
 
